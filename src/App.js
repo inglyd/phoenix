b72571ddbb38e37373af8492132acc5ed6183fd6
inglyd <inglydalves@outlook.com>
2021-11-08 01:13:18 -0300

    estrutura do projeto

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Layout from './components/layout';
+
+function App() {
+  return (
+    <>
+      <main>
+        <Layout>
+          <p>test</p>
+        </Layout>
+      </main>
+    </>
+  );
+}
+
+export default App;
