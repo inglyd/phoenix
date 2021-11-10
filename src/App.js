eede235387d53273d9db7a0f64340859b76617fb
inglyd <inglydalves@outlook.com>
2021-11-09 21:50:50 -0300

    estruturando o projeto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,29 @@ function App() {
     <>
       <main>
         <Layout>
-          <p>test</p>
+          <div>
+            <img src="https://avatars.githubusercontent.com/u/33432680?v=4" alt="Avatar of user" />
+            <h1>Inglyd Maria Alves</h1>
+            <h3>Username:</h3>
+            <span>inglyd</span>
+            <div>
+              <div>
+                <h4>Followers</h4>
+                <span>5</span>
+              </div>
+              <div>
+                <h4>Repositories</h4>
+                <span>5</span>
+              </div>
+              <div>
+                <h4>Starreds</h4>
+                <span>5</span>
+              </div>
+            </div>
+          </div>
+          <div>Users</div>
+          <div>Repositories</div>
+          <div>Starred</div>
         </Layout>
       </main>
     </>
